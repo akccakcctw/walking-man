890d9fe09a2c9ffd6911e4e37a9ca49eb62345bf
Rex Tsou <akccakccwww@gmail.com>
2017-09-07 15:48:33 +0800

    Update layout style

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('walking man sample');
+  var myUrl = 'https://akccakcctw.github.io';
+  var BOHAN = 'BOHAN Graphic Design Office';
+  var urlBOHAN = 'https://goo.gl/bkAxv1';
+  var styleConsole = '\n    background: #131313;\n    color: #B1F1F1;\n    font-size: 18px;\n    border-bottom: 1px solid transparent;\n    padding: .1rem .5rem;\n  ';
+  var styleConsole2 = '\n    background: #131313;\n    color: #DFF1F1;\n    font-size: 12px;\n    border-bottom: 1px solid transparent;\n    padding: .1rem .5rem;\n  ';
+  console.log('%cAuthor: Rex Tsou(akccakcctw), ' + myUrl, styleConsole);
+  console.log('%cImage Credit: ' + BOHAN + ', ' + urlBOHAN, styleConsole2);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('walking man sample');
+  const myUrl = 'https://akccakcctw.github.io';
+  const BOHAN = 'BOHAN Graphic Design Office';
+  const urlBOHAN = 'https://goo.gl/bkAxv1';
+  const styleConsole = `
+    background: #131313;
+    color: #B1F1F1;
+    font-size: 18px;
+    border-bottom: 1px solid transparent;
+    padding: .1rem .5rem;
+  `;
+  const styleConsole2 = `
+    background: #131313;
+    color: #DFF1F1;
+    font-size: 12px;
+    border-bottom: 1px solid transparent;
+    padding: .1rem .5rem;
+  `;
+  console.log(`%cAuthor: Rex Tsou(akccakcctw), ${myUrl}`, styleConsole);
+  console.log(`%cImage Credit: ${BOHAN}, ${urlBOHAN}`, styleConsole2);
 });
